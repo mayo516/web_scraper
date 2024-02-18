105bd31910c8590dd744eb0c2960ff4ba8c81a34
DESKTOP-6577HCU\dmsgk <dmsgk2323@naver.com>
2024-02-18 18:41:25 +0900

    형태

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ c = 11
 print(c)
 
 my_age = 88
+
+my_name = "eunha"
+my_name = 'eunha2'
+my_age = "29"
+print(my_age)
+
+is_true = True
+
+print(is_true)
+
+print("안녕하세요 저는", my_name, "이고", my_age, "살 입니다!")
